c05a1c94df92429b763bfb9c36edf96679ea9480
Erdoo <erdoo.kuhwa@gmail.com>
2023-02-20 16:09:19 -0600

    + config for s3, lambda fxn, api gw, ddb table, CW logs, IAM roles & policies

---
+++ main.tf
@@ -0,0 +1,261 @@
+data "archive_file" "api_code_archive" {
+  type        = "zip"
+  source_file = "./lambda_function.py"
+  output_path = "bootstrap.zip"
+}
+
+resource "aws_s3_bucket" "api_bucket" {
+  bucket        = "erdoo-api-bucket-archives"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "api_code_archive" {
+  bucket = aws_s3_bucket.api_bucket.id
+  key    = "bootstrap.zip"
+  source = data.archive_file.api_code_archive.output_path
+  etag   = filemd5(data.archive_file.api_code_archive.output_path)
+
+  lifecycle {
+    ignore_changes = [
+      etag,
+      version_id
+    ]
+  }
+}
+
+resource "aws_dynamodb_table" "my_table" {
+  name         = "erdoo-visitor-table"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "user_id"
+    type = "N"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "N"
+  }
+
+  attribute {
+    name = "count"
+    type = "N"
+  }
+
+  hash_key  = "user_id"
+  range_key = "timestamp"
+
+  local_secondary_index {
+    name            = "my_LSI"
+    range_key       = "count"
+    projection_type = "ALL"
+  }
+}
+
+resource "aws_lambda_function" "api_lambda" {
+  function_name    = "example-api"
+  role             = aws_iam_role.api_lambda_role.arn
+  s3_bucket        = aws_s3_bucket.api_bucket.id
+  s3_key           = aws_s3_bucket_object.api_code_archive.key
+  source_code_hash = data.archive_file.api_code_archive.output_base64sha256
+  architectures    = ["arm64"]
+  runtime          = "provided.al2"
+  handler          = "bootstrap"
+  memory_size      = 128
+  publish          = true
+
+  lifecycle {
+    ignore_changes = [
+      last_modified,
+      source_code_hash,
+      version,
+      environment
+    ]
+  }
+}
+
+resource "aws_iam_role" "api_lambda_role" {
+  name = "example-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+
+  # Update aws_iam_role to grant get DynamoDB access to the DDB table
+  inline_policy {
+    name = "DynamoDBAccess"
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action = [
+            "dynamodb:GetItem",
+            "dynamodb:BatchGetItem",
+            "dynamodb:Query",
+            "dynamodb:PutItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem",
+            "dynamodb:BatchWriteItem"
+          ],
+          Effect : "Allow"
+          Resource : "${aws_dynamodb_table.my_table.arn}"
+        }
+      ]
+    })
+  }
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
+}
+
+resource "aws_lambda_alias" "api_lambda_alias" {
+  name             = "production"
+  function_name    = aws_lambda_function.api_lambda.arn
+  function_version = "$LATEST"
+
+  lifecycle {
+    ignore_changes = [
+      function_version
+    ]
+  }
+}
+
+resource "aws_cloudwatch_log_group" "api_lambda_group" {
+  name              = "/aws/lambda/${aws_lambda_function.api_lambda.function_name}"
+  retention_in_days = 14
+  tags              = {}
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda-role-policy"
+  role = aws_iam_role.api_lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_apigatewayv2_api" "api_gateway" {
+  name          = "example-api-gateway"
+  protocol_type = "HTTP"
+  tags          = {}
+}
+
+resource "aws_cloudwatch_log_group" "api_gateway_log_group" {
+  name              = "/aws/api_gateway_log_group/${aws_apigatewayv2_api.api_gateway.name}"
+  retention_in_days = 14
+  tags              = {}
+}
+
+resource "aws_apigatewayv2_stage" "api_gateway_default_stage" {
+  api_id      = aws_apigatewayv2_api.api_gateway.id
+  name        = "$default"
+  auto_deploy = true
+  tags        = {}
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gateway_log_group.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      status                  = "$context.status"
+      responseLatency         = "$context.responseLatency"
+      path                    = "$context.path"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    })
+  }
+}
+
+resource "aws_apigatewayv2_integration" "api_gateway_integration" {
+  api_id             = aws_apigatewayv2_api.api_gateway.id
+  integration_uri    = "${aws_lambda_function.api_lambda.arn}:${aws_lambda_alias.api_lambda_alias.name}"
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  request_parameters = {}
+  request_templates  = {}
+}
+
+resource "aws_apigatewayv2_route" "api_gateway_any_route" {
+  api_id               = aws_apigatewayv2_api.api_gateway.id
+  route_key            = "ANY /{proxy+}"
+  target               = "integrations/${aws_apigatewayv2_integration.api_gateway_integration.id}"
+  authorization_scopes = []
+  request_models       = {}
+}
+
+resource "aws_lambda_permission" "api_gateway_lambda_permission" {
+  principal     = "apigateway.amazonaws.com"
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_lambda.function_name
+  qualifier     = aws_lambda_alias.api_lambda_alias.name
+  source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
+}
+
+output "api_gateway_invoke_url" {
+  description = "API Gateway default stage invocation URL"
+  value       = aws_apigatewayv2_stage.api_gateway_default_stage.invoke_url
+}
+
+# Role for api gateway
+resource "aws_iam_role" "api_gateway_role" {
+  name = "api_gateway_role"
+
+  assume_role_policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "apigateway.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+    EOF
+}
+
+# Grant lambda access to the S3 bucket
+resource "aws_s3_bucket_policy" "lambda_access" {
+  bucket = aws_s3_bucket.api_bucket.id
+
+  policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "AllowLambdaFunctionAccess",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Action": "s3:*",
+                "Resource": "arn:aws:s3:::erdoo-api-bucket-archives/*"
+            }
+        ]
+    }
+    EOF
+}
